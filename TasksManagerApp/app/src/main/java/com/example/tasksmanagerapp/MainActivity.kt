337cfd634907af757e17fe41d64bac81e6771920
kairosb <kairomatheussb@gmail.com>
2025-01-08 18:00:27 -0300

    TasksManagerApp

---
+++ TasksManagerApp/app/src/main/java/com/example/tasksmanagerapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tasksmanagerapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tasksmanagerapp.ui.screens.TasksScreen
+import com.example.tasksmanagerapp.ui.theme.TasksManagerAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                TasksScreen()
+            }
+        }
+    }
+}
+
